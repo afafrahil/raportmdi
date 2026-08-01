640042eee1e0429f26b0ca610c821f97a37cfbb8
afafrahil <fataberkas@gmail.com>
2026-08-01 10:19:07 +0700

    Update API_URL to the new web app URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Tempel URL hasil "Deploy > Web app" dari Apps Script Anda di sini.
 // Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
-const API_URL = "TEMPEL_URL_WEB_APP_APPS_SCRIPT_ANDA_DI_SINI";
+const API_URL = "https://script.google.com/macros/s/AKfycbzUXMyLcSJKfhadZviRp429CyOXPvvVqg4H4nWFaUjYtcNt1nTs8VTiRR4jYOfO5O4MXw/exec";
