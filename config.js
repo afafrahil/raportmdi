0613256c40863ca042d33c68b2065663f144c7bc
afafrahil <fataberkas@gmail.com>
2026-08-01 10:12:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Tempel URL hasil "Deploy > Web app" dari Apps Script Anda di sini.
+// Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+const API_URL = "TEMPEL_URL_WEB_APP_APPS_SCRIPT_ANDA_DI_SINI";
