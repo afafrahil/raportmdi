f6d55c452fb7f8186fc6923f62219ead74b8b3cf
afafrahil <fataberkas@gmail.com>
2026-08-01 10:27:44 +0700

    Clean up config.js by removing comments
    Removed commented-out lines for clarity.

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Tempel URL hasil "Deploy > Web app" dari Apps Script Anda di sini.
-// Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 const API_URL = "https://script.google.com/macros/s/AKfycbzUXMyLcSJKfhadZviRp429CyOXPvvVqg4H4nWFaUjYtcNt1nTs8VTiRR4jYOfO5O4MXw/exec";
